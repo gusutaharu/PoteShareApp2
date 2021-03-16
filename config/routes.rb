428b11822071eca1472acd0de4a732db5261da54
Ubuntu <ubuntu@ip-172-31-43-108.us-east-2.compute.internal>
2021-03-16 03:20:24 +0000

    finished_login_signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get '/' => 'home#top'
+  
+  get 'users/account' => 'users#account'
+  
+  devise_for :users
+
+  root to: 'home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

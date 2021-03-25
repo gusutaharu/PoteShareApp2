26e7d1241ddbe3284ba03d5d010efaae666d2b7d
Ubuntu <ubuntu@ip-172-31-43-108.us-east-2.compute.internal>
2021-03-25 16:36:46 +0000

    side_menu_finished

---
+++ app/javascript/side_menu.js
@@ -0,0 +1,19 @@
+/*$(function() {
+2
+    $('.user-nav li').each(function(){
+3
+        var $href = $(this).attr('href');
+4
+        if(location.href.match($href)) {
+5
+            $(this).addClass('active');
+6
+        } else {
+7
+            $(this).removeClass('active');
+8
+        }
+9
+    });
+10
+});*/
